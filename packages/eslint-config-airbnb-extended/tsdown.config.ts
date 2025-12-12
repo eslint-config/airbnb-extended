f1f2b252b1c0cc769cc4cc64e1a3ba202961f063
Nisharg Shah <nishargshah3101@gmail.com>
2025-12-13 01:01:11 +0530

    feat: tsconfig changed based on the configurations

--- packages/eslint-config-airbnb-extended/tsdown.config.ts
+++ packages/eslint-config-airbnb-extended/tsdown.config.ts
@@ -1,18 +1,30 @@
 import { defineConfig } from 'tsdown';
 
+import type { UserConfig } from 'tsdown';
+
 export default defineConfig((options) => {
   const { watch } = options;
 
-  return {
-    entry: ['index.ts', 'legacy.ts'],
+  const common = {
     dts: true,
-    format: ['cjs', 'esm'],
     outDir: 'dist',
-    platform: 'browser',
+    platform: 'node',
     treeshake: !watch,
     minify: !watch,
-    exports: true,
     nodeProtocol: true,
     unbundle: true,
-  };
+  } satisfies UserConfig;
+
+  return [
+    {
+      ...common,
+      entry: 'index.ts',
+      format: ['cjs', 'esm'],
+    },
+    {
+      ...common,
+      entry: 'legacy.ts',
+      format: 'cjs',
+    },
+  ];
 });
