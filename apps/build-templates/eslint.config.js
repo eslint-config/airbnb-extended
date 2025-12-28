c116a50d259cb92102f81780dd34cc49b78a76c0
Nisharg Shah <nishargshah3101@gmail.com>
2025-12-29 00:48:36 +0530

    fix: build templates

--- apps/build-templates/eslint.config.js
+++ apps/build-templates/eslint.config.js
@@ -1,14 +1,15 @@
 import config from '@airbnb-extended/eslint-config/base';
-import { defineConfig } from 'eslint/config';
+import { defineConfig } from '@airbnb-extended/eslint-config/utils';
 
 export default defineConfig([
   ...config,
   // Disable Dependencies Import Issue for Templates ESLint Files
-  // {
-  //   name: 'x/import-x/disable-extraneous-deps',
-  //   files: ['templates/**/eslint.config.mjs'],
-  //   rules: {
-  //     'import-x/no-extraneous-dependencies': 'off',
-  //   },
-  // },
+  {
+    name: 'x/import-x/disable-rules',
+    files: ['templates/**/eslint.config.mjs'],
+    rules: {
+      'import-x/no-unresolved': 'off',
+      'import-x/no-extraneous-dependencies': 'off',
+    },
+  },
 ]);
