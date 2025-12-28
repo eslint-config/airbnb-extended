6cd91037d813463eb8f601920bbc467833620d6c
Nisharg Shah <nishargshah3101@gmail.com>
2025-12-28 21:24:53 +0530

    feat: prettier config added

---
+++ configs/prettier-config/eslint.config.js
@@ -0,0 +1,3 @@
+import config from '../eslint-config/src/eslint-internal.config.js';
+
+export default [...config];
---
+++ configs/prettier-config/lint-staged.config.js
@@ -0,0 +1,3 @@
+import config from '../lint-staged-config/src/lint-staged-base.config.js';
+
+export default { ...config };
