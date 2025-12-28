b9d9f6e959f5190165ba1315061ed69b6837c065
Nisharg Shah <nishargshah3101@gmail.com>
2025-12-29 01:13:52 +0530

    fix: docs

--- docs/eslint.config.js
+++ docs/eslint.config.js
@@ -1,14 +1,19 @@
 import config from '@airbnb-extended/eslint-config/base';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, getDevDepsList } from '@airbnb-extended/eslint-config/utils';
 
 export default defineConfig([
   ...config,
-  // Disable Dependencies Import Issue for Templates ESLint Files
-  // {
-  //   name: 'x/import-x/disable-extraneous-deps',
-  //   files: ['docs/**/*.{ts,cts,mts,tsx}'],
-  //   rules: {
-  //     'import-x/no-extraneous-dependencies': 'off',
-  //   },
-  // },
+  {
+    name: 'x/import-x/modify-no-extraneous-dependencies',
+    rules: {
+      'import-x/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: getDevDepsList('typescript'),
+          optionalDependencies: false,
+          whitelist: ['vitepress', 'vitepress-plugin-group-icons'],
+        },
+      ],
+    },
+  },
 ]);
