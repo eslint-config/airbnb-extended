08b591fe64fb66124bde6f327844e2f1f505d49b
Nisharg Shah <nishargshah3101@gmail.com>
2025-12-29 01:23:14 +0530

    fix: root

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
 import config from '@airbnb-extended/eslint-config/root';
-import { defineConfig } from 'eslint/config';
+import { defineConfig } from '@airbnb-extended/eslint-config/utils';
 
 export default defineConfig(config);
