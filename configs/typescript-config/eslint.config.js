d87bdf19d1e7c8f9cdd0b018d15fe136d4f12502
Nisharg Shah <nishargshah3101@gmail.com>
2025-12-29 01:26:13 +0530

    fix: typescript-config

--- configs/typescript-config/eslint.config.js
+++ configs/typescript-config/eslint.config.js
@@ -1,4 +1,4 @@
 import config from '@airbnb-extended/eslint-config/internal';
-import { defineConfig } from 'eslint/config';
+import { defineConfig } from '@airbnb-extended/eslint-config/utils';
 
 export default defineConfig(config);
