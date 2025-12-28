a18361c8151ea0a782987ecbe27eccb73ca0d520
Nisharg Shah <nishargshah3101@gmail.com>
2025-12-29 01:20:58 +0530

    fix: airbnb-config-eslint-extended

--- packages/eslint-config-airbnb-extended/eslint.config.js
+++ packages/eslint-config-airbnb-extended/eslint.config.js
@@ -1,4 +1,4 @@
 import config from '@airbnb-extended/eslint-config/base';
-import { defineConfig } from 'eslint/config';
+import { defineConfig } from '@airbnb-extended/eslint-config/utils';
 
 export default defineConfig(config);
