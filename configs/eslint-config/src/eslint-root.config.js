b78f86da7aea3b2cd37a1a4aac1cf896a1348464
Nisharg Shah <nishargshah3101@gmail.com>
2025-12-31 00:27:38 +0530

    fix: added root folders

--- configs/eslint-config/src/eslint-root.config.js
+++ configs/eslint-config/src/eslint-root.config.js
@@ -1,10 +1,17 @@
+import { readdirSync } from 'node:fs';
+
 import config from './eslint-base.config.js';
-import { defineConfig, globalIgnores } from './eslint-utils.js';
+import { defineConfig, gitIgnoreFile, globalIgnores, rootPath } from './eslint-utils.js';
+
+export const rootFolders = readdirSync(rootPath, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map((dirent) => `**/${dirent.name}`)
+  .filter((folder) => !gitIgnoreFile.ignores.includes(folder));
 
 export default defineConfig([
   // Ignores every everything except root files
   {
-    ...globalIgnores(['apps/**', 'configs/**', 'docs/**', 'packages/**']),
+    ...globalIgnores(rootFolders),
     name: 'x/ignore-everything-except-root-files',
   },
   ...config,
--- configs/eslint-config/src/eslint-utils.js
+++ configs/eslint-config/src/eslint-utils.js
@@ -6,14 +6,9 @@ import { helpers } from 'eslint-config-airbnb-extended';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const rootPath = path.resolve(dirname, '..', '..', '..');
+export const rootPath = path.resolve(dirname, '..', '..', '..');
 
 export const gitIgnoreFile = includeIgnoreFile(path.resolve(rootPath, '.gitignore'));
-
-// export const rootFolders = readdirSync(rootPath, { withFileTypes: true })
-//   .filter((dirent) => dirent.isDirectory())
-//   .map((dirent) => `**/${dirent.name}`)
-//   .filter((folder) => !gitIgnoreFile.ignores.includes(folder));
 
 export const { getDevDepsList } = helpers;
 
